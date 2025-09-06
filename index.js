e6605734acd94feb79dbd24dae34c8a0c141ebe5
D-Honoured1 <jamesalvez347@gmail.com>
2025-09-06 19:17:06 +0100

    Update: index

--- index.js
+++ index.js
@@ -100,11 +100,12 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-    },
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  sameSite: "lax",
+},
+
   }),
 )
 
